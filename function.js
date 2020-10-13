27cc52fa4b58ca502d1f9a8aa4b253c2db5eebc8
restuu <restu_u@outlook.com>
2020-10-13 21:41:52 +0700

    Feat: Init deploy firebase. TODO: Upgrade to nodejs10

---
+++ function.js
@@ -0,0 +1,33 @@
+const functions = require("firebase-functions")
+// hack for firebase functions nodejs8
+global.URL = require('url').URL;
+
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({
+  dev,
+  conf: {
+    distDir: ".next"
+  }
+})
+
+const handle = app.getRequestHandler()
+
+exports.nextServer = functions.https.onRequest((req, res) => {
+  return app.prepare().then(() => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query)
+    } else {
+      handle(req, res, parsedUrl)
+    }
+  })
+})
--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  webpack: (config, option) => {
-    config.module.rules.push({
-      test: /\.(ttf|woff|woff2)$/,
-      loader: 'url-loader',
-      options: {
-        limit: 10000,
-      },
-    });
-    // Important: return the modified config
-    return config;
-  },
+  // webpack: (config, option) => {
+  //   config.module.rules.push({
+  //     test: /\.(ttf|woff|woff2)$/,
+  //     loader: 'url-loader',
+  //     options: {
+  //       limit: 10000,
+  //     },
+  //   });
+  //   // Important: return the modified config
+  //   return config;
+  // },
 };
